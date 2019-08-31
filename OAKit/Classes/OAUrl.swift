5896d836e2a2a11ccd834eab40c14be038a699bc
OA Wu <comdan66@gmail.com>
2019-09-01 01:09:26 +0800

    Fix code

--- OAKit/Classes/OAUrl.swift
+++ OAKit/Classes/OAUrl.swift
@@ -19,7 +19,12 @@ public class UrlGet {
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard error == nil else { return closure(.failure(0, error!.localizedDescription)) }
             guard let httpResponse = response as? HTTPURLResponse else { return closure(.failure(0, "回應錯誤！")) }
-            guard httpResponse.statusCode == 200 else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！")) }
+            guard httpResponse.statusCode == 200 else {
+                guard let data！ = data else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！")) }
+                guard let json = try? JSONSerialization.jsonObject(with: data！, options: []) else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！訊息：" + String(decoding: data！, as: UTF8.self))) }
+                guard let jsonData = json as? [String: [String]], let messages = jsonData["messages"] else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！訊息：" + String(decoding: data！, as: UTF8.self))) }
+                return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！訊息：" + messages.joined(separator: "")))
+            }
             guard let mime = response?.mimeType, mime == "application/json" else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "格式不是 Json！")) }
             guard let json = try? JSONSerialization.jsonObject(with: data!, options: []) else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "格式錯誤！")) }
             return closure(.success(json))
@@ -40,7 +45,12 @@ public class UrlPost {
         URLSession.shared.dataTask(with: request) { data, response, error in
             guard error == nil else { return closure(.failure(0, error!.localizedDescription)) }
             guard let httpResponse = response as? HTTPURLResponse else { return closure(.failure(0, "回應錯誤！")) }
-            guard httpResponse.statusCode == 200 else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！")) }
+            guard httpResponse.statusCode == 200 else {
+                guard let data！ = data else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！")) }
+                guard let json = try? JSONSerialization.jsonObject(with: data！, options: []) else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！訊息：" + String(decoding: data！, as: UTF8.self))) }
+                guard let jsonData = json as? [String: [String]], let messages = jsonData["messages"] else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！訊息：" + String(decoding: data！, as: UTF8.self))) }
+                return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "狀態碼錯誤！訊息：" + messages.joined(separator: "")))
+            }
             guard let mime = response?.mimeType, mime == "application/json" else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "格式不是 Json！")) }
             guard let json = try? JSONSerialization.jsonObject(with: data!, options: []) else { return closure(.failure(UInt16(truncatingIfNeeded: httpResponse.statusCode), "格式錯誤！")) }
             return closure(.success(json))
