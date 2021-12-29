b38d58a6e0a2f9f725fc706f5ccd65034d698873
OA Wu <oa.wu@idrip.coffee>
2021-12-29 16:22:38 +0800

    fix code

--- Example/OAKit/AppDelegate.swift
+++ Example/OAKit/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window!.backgroundColor = UIColor.white
-        self.window!.rootViewController = UINavigationController(rootViewController: ViewController())
+        self.window!.rootViewController = UINavigationController(rootViewController: MainController())
         self.window!.makeKeyAndVisible()
         
         return true
